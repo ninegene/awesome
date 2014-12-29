45f0649db9a6b30251a21cda825a01079751f6fd
aung <aung@macdev.local>
2014-12-28 21:46:37 -0800

    script to update to latest commit of submodules

--- update.sh
+++ update.sh
@@ -3,3 +3,10 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 git submodule update --init --recursive
+git submodule foreach git pull origin master
+
+if [ "$1" = "remote" ]; then
+    git add --all
+    git commit -m "Update submodule(s) to latest commit"
+    git push origin master
+fi
